550484c4eac8378db271c2074f2ce8597a50f26f
Joel <joel@expanse.com>
2023-10-17 09:48:08 +1300

    Updated SDK

--- index.mjs
+++ index.mjs
@@ -1,43 +1,52 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import core from "@actions/core";
-import fs from "fs";
-import path from "path";
+import core from "@actions/core"
+import fs from "fs"
+import path from "path"
+
+var aws = require("aws-sdk")
+var mime = require("mime")
 
 try {
-    const endpoint = core.getInput('endpoint');
-    const accesskeyid = core.getInput('accesskeyid');
-    const secretaccesskey = core.getInput('secretaccesskey');
-    const bucket = core.getInput('bucket');
-    const file = core.getInput('file');
-    const destination = core.getInput('destination');
-    const credentials = {
-        accessKeyId: accesskeyid,
-        secretAccessKey: secretaccesskey,
+  const endpoint = core.getInput("endpoint")
+  const accesskeyid = core.getInput("accesskeyid")
+  const secretaccesskey = core.getInput("secretaccesskey")
+  const bucket = core.getInput("bucket")
+  const file = core.getInput("file")
+  const destination = core.getInput("destination")
+
+  var s3 = new aws.S3({
+    accessKeyId: accesskeyid,
+    secretAccessKey: secretaccesskey,
+    endpoint: endpoint + destination,
+  })
+
+  var fileStream = fs.createReadStream(file)
+
+  fileStream.on("error", function (err) {
+    console.log("File Error", err)
+    core.setFailed(err)
+  })
+
+  let key = ""
+  if (destination == "") {
+    key = path.basename(file)
+  } else {
+    key = destination
+  }
+
+  let params = {
+    Bucket: bucket,
+    Key: key,
+    Body: fs.readFileSync(file),
+    ContentType: mime.getType(file),
+  }
+
+  s3.putObject(params, function (err, data) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Successfully uploaded " + destination + " to " + bucket)
     }
-    const client = new S3Client({
-        endpoint: endpoint,
-        credentials: credentials,
-        region: null
-    });
-    var fileStream = fs.createReadStream(file);
-    fileStream.on('error', function (err) {
-        console.log('File Error', err);
-        core.setFailed(err);
-    });
-    let key = "";
-    if (destination == "") {
-        key = path.basename(file);
-    } else { key = destination; }
-    const command = new PutObjectCommand({
-        Bucket: bucket,
-        Key: key,
-        Body: fileStream,
-    });
-    try {
-        await client.send(command);
-    } catch (err) {
-        core.setFailed(err);
-    }
+  })
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message)
 }
